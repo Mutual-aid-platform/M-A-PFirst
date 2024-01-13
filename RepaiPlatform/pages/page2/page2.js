ae2d7546efeb7c0e7baea175f6b451be3f175ccd
502scanf <2131897300@qq.com>
2024-01-13 21:44:21 +0800

    last

--- RepaiPlatform/pages/page2/page2.js
+++ RepaiPlatform/pages/page2/page2.js
@@ -5,62 +5,33 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+     tabs:[
+       {
+         id: 0,
+         value: "处理中",
+         isActive: true
+       },
+       {
+        id: 1,
+        value: "已完成",
+        isActive: false
+      }
+     ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
+  handleTabsItemChange(e){
+    const {index}=e.detail;
+    let {tabs}=this.data;
+    tabs.forEach((v,i)=>i===index?v.isActive=true:v.isActive=false);
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+    this.setData({
+      tabs
+    })
   }
 })
