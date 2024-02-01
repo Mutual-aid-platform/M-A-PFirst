2fa7db3789d08490b93907a336deb29eb4ec0028
502scanf <2131897300@qq.com>
2024-02-01 17:20:19 +0800

    初次后端代码

---
+++ mapPlatForm/src/main/java/com/wx2work/mapplatform/pojo/mapUser.java
@@ -0,0 +1,20 @@
+package com.wx2work.mapplatform.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class mapUser {
+    private int id;
+    private String name;
+    private String wxName;
+    private String orderDetail;
+    private LocalDate orderTime;
+    private int orderDone;
+}
---
+++ mapPlatForm/src/test/java/com/wx2work/mapplatform/MapPlatFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wx2work.mapplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapPlatFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
